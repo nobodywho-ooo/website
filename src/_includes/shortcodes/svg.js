7e98dd1432e1c0b35fd9a6ea44fcacfed6ead1c1
Pierre Bresson <262128333+pielouNW@users.noreply.github.com>
2026-04-01 11:21:12 +0200

    add colors to bento

--- src/_includes/shortcodes/svg.js
+++ src/_includes/shortcodes/svg.js
@@ -237,18 +237,36 @@ const svg = (name, className, gradient) => {
   }
 
   if (name == "bolt") {
+    const gradientDef = gradient ? `
+        <defs>
+          <linearGradient id="grad-bolt" x1="0%" y1="0%" x2="100%" y2="100%">
+            <stop offset="0%" stop-color="${gradient[0]}"/>
+            <stop offset="100%" stop-color="${gradient[1]}"/>
+          </linearGradient>
+        </defs>` : '';
+    const strokeColor = gradient ? 'url(#grad-bolt)' : 'currentColor';
     icon = `
-      <svg role="img" class="${className}" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <svg role="img" class="${className}" fill="none" stroke-width="1.5" stroke="${strokeColor}" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <title>Bolt</title>
+        ${gradientDef}
         <path stroke-linecap="round" stroke-linejoin="round" d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z" />
       </svg>
     `;
   }
 
   if (name == "lock") {
+    const gradientDef = gradient ? `
+        <defs>
+          <linearGradient id="grad-lock" x1="0%" y1="0%" x2="100%" y2="100%">
+            <stop offset="0%" stop-color="${gradient[0]}"/>
+            <stop offset="100%" stop-color="${gradient[1]}"/>
+          </linearGradient>
+        </defs>` : '';
+    const strokeColor = gradient ? 'url(#grad-lock)' : 'currentColor';
     icon = `
-      <svg role="img" class="${className}" fill="none"  stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <svg role="img" class="${className}" fill="none" stroke-width="1.5" stroke="${strokeColor}" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <title>Lock</title>
+        ${gradientDef}
         <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
       </svg>
     `;
