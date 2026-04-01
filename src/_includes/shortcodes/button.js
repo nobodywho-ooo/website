a194e9cf141103dd13ce4c20d54f4e4892547c80
Pierre Bresson <262128333+pielouNW@users.noreply.github.com>
2026-04-01 15:36:22 +0200

    fix font color and menu

--- src/_includes/shortcodes/button.js
+++ src/_includes/shortcodes/button.js
@@ -9,7 +9,7 @@ const button = (text, link, type) => {
   var customClass = "text-white bg-primary-500 hover:bg-primary-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600 dark:shadow-none dark:hover:bg-primary-400 dark:focus-visible:outline-primary-500";
 
   if (type == "secondary") {
-    customClass ="bg-white text-gray-900 inset-ring inset-ring-gray-300 hover:bg-gray-50 dark:bg-white/10 dark:text-white dark:shadow-none dark:inset-ring-white/5 dark:hover:bg-white/20";
+    customClass ="bg-white text-neutral-900 inset-ring inset-ring-gray-300 hover:bg-gray-50 dark:bg-white/10 dark:text-white dark:shadow-none dark:inset-ring-white/5 dark:hover:bg-white/20";
   }
 
   return `<a href="${link}" class="${baseClass} ${customClass}">${text}</a>`;
