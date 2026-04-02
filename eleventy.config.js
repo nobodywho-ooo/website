352ebdcfb12a4f9a5a0d295e0b3f6dd3c588abbd
AsbjornOlling <asbjornolling@gmail.com>
2026-04-02 17:16:01 +0200

    add .well-known dir to export

--- eleventy.config.js
+++ eleventy.config.js
@@ -15,6 +15,7 @@ export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/favicon");
   eleventyConfig.addPassthroughCopy("src/assets/fonts");
   eleventyConfig.addPassthroughCopy({ "src/robots.txt": "/robots.txt" });
+  eleventyConfig.addPassthroughCopy({ ".well-known": "/.well-known" });
   eleventyConfig.addPassthroughCopy("src/assets/css/tailwind-dist.css");
   eleventyConfig.addPassthroughCopy("src/assets/css/prism-dark.css");
   eleventyConfig.addPassthroughCopy("src/assets/css/terminal.css");
